eb43935e1c7499676c70580e29ae1b977484b5b1
Mylinde <mameogdoay@posteo.de>
2024-03-24 14:45:52 +0100

    Update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '16'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '10'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:1338')
