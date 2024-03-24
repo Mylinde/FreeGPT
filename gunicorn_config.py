c9eed144387ec2fb7ef86aff01b593ffb7d01d2e
Mylinde <mameogdoay@posteo.de>
2024-03-24 12:04:37 +0100

    Update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
-threads = int(os.environ.get('GUNICORN_THREADS', '8'))
+threads = int(os.environ.get('GUNICORN_THREADS', '16'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '10'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:1338')
 forwarded_allow_ips = '*'
