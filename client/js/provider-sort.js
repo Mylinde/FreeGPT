77e65bb2037c70003850a53463a2a2a315dc1104
Mylinde <mameogdoay@posteo.de>
2024-03-29 01:17:09 +0100

    Only providers belonging to the model are displayed in the provider selection

---
+++ client/js/provider-sort.js
@@ -0,0 +1,37 @@
+const modelSelect = document.getElementById('model');
+const providerSelect = document.getElementById('provider');
+
+function showAllOptions() {
+  Array.from(providerSelect.options).forEach(option => {
+    option.style.display = "block";
+  });
+}
+
+modelSelect.addEventListener('change', () => {
+  showAllOptions();
+
+const selectedModel = modelSelect.value;
+const hiddenOptions = ['g4f.Provider.Llama2', 'g4f.Provider.PerplexityLab', 'g4f.Provider.Bing', 'g4f.Provider.Liaobots', 'g4f.Provider.ChatgptLogin', 'g4f.Provider.Phind'];
+  
+  if (selectedModel === 'gpt-3.5-turbo') {
+    providerSelect.value = 'g4f.Provider.Auto';
+    hideOptions(hiddenOptions.filter(option => ['g4f.Provider.ChatgptLogin'].indexOf(option) === -1));
+  } else if (selectedModel === 'gpt-4') {
+    providerSelect.value = 'g4f.Provider.Auto';
+    hideOptions(hiddenOptions.filter(option => ['g4f.Provider.Bing', 'g4f.Provider.Liaobots', 'g4f.Provider.Phind'].indexOf(option) === -1));
+  } else if (selectedModel === 'llama2-70b') {
+    providerSelect.value = 'g4f.Provider.Auto';
+    hideOptions(hiddenOptions.filter(option => ['g4f.Provider.Llama2', 'g4f.Provider.PerplexityLab'].indexOf(option) === -1));
+  } else if (selectedModel === 'mixtral-8x7b') {
+    providerSelect.value = 'g4f.Provider.Auto';
+    hideOptions(hiddenOptions.filter(option => ['g4f.Provider.PerplexityLab'].indexOf(option) === -1));
+  }
+});
+
+function hideOptions(optionsToHide) {
+  optionsToHide.forEach(option => {
+    const optionElement = providerSelect.querySelector(`option[value="${option}"]`);
+    optionElement.style.display = "none";
+  })
+}
+  
