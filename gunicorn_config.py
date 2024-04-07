8a65fc34aa01d8e2c90ec7c2bc8a0dc5abf52a9d
Mylinde <mameogdoay@posteo.de>
2024-04-07 22:51:16 +0200

    Update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,3 +6,4 @@ timeout = int(os.environ.get('GUNICORN_TIMEOUT', '10'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:1338')
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+ssl_version = 3
