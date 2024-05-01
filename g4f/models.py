8833275d3a082db22d76adf03f8f113014bf574b
Mylinde <mameogdoay@posteo.de>
2024-05-02 00:25:14 +0200

    Update

--- g4f/models.py
+++ g4f/models.py
@@ -74,7 +74,7 @@ llama2_13b = Model(
 )
 
 llama2_70b = Model(
-    name          = "meta/llama-2-70b",
+    name          = "meta-llama/Llama-2-70b-chat",
     base_provider = "meta",
     best_provider = RetryProvider([Llama, DeepInfra])
 )
