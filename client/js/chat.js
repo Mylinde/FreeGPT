62536a0ca673682c7678b9896b84332f59724e88
Mylinde <mameogdoay@posteo.de>
2024-05-09 00:01:05 +0200

    Update

--- client/js/chat.js
+++ client/js/chat.js
@@ -450,17 +450,7 @@ window.onload = async () => {
 	message_input.addEventListener("keydown", async (evt) => {
 		if (prompt_lock) return;
 	
-		if (evt.key === "Enter" && !evt.shiftKey) {
-			evt.preventDefault();
-			await handle_ask();
-		}
-			
-		if (evt.key === "Enter" && !(device.platform === "Android" && device.keyboard?.isVirtual)) {
-			evt.preventDefault();
-			await handle_ask();
-		}
-			
-		if (evt.key === "Enter" && !(device.platform === "iOS" && evt.Code === 133)) {
+		if (evt.key === "Enter" && !evt.shiftKey && !(device.platform === "Android" && device.keyboard?.isVirtual) && !(device.platform === "iOS" && evt.Code === 133) ) {
 			evt.preventDefault();
 			await handle_ask();
 		}
